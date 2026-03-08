f299232bf50a6b6ba5bd2010da4c09975afb8563
Nilton <nilton@example.com>
2026-03-08 21:42:30 +0000

    fix(deploy): setup vercel/react-router preset for ssr support

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,14 @@ import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [
+    tailwindcss(),
+    reactRouter({
+      presets: [vercelPreset()]
+    }),
+    tsconfigPaths()
+  ],
 });
